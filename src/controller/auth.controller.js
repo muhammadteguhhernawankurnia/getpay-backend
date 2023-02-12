e01a7fa1c43844c6166e7a6a2e0f1d5b1e4785c8
muhammadteguhhernawankurnia <moch.teguhhernawankurnia@gmail.com>
2023-02-12 11:13:07 +0700

    add jwt and bcrypt

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -2,33 +2,86 @@
 //import transaction model
 const authModel = require('../model/auth.model');
 
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const { JWT_PRIVATE_KEY } = process.env;
+
 const authController = {
   login: (req, res) => {
+    //tanpa jwt bcrypt
+    // return authModel
+    //   .login(req.body)
+    //   .then((result) => {
+    //     return res.status(200).send({
+    //       message: 'success',
+    //       data: {
+    //         user: result,
+    //         token: 'ssdfsdmkmk',
+    //       },
+    //     });
+    //   })
+    //   .catch((err) => {
+    //     return res.status(500).send({ message: 'username/password salah' });
+    //   });
     return authModel
       .login(req.body)
       .then((result) => {
-        return res.status(200).send({
-          message: 'success',
-          data: {
-            user: result,
-            token: 'ssdfsdmkmk',
-          },
-        });
+        jwt.sign(
+          { id: result.id, role: result.role },
+          JWT_PRIVATE_KEY,
+          { expiresIn: '1d' },
+          (err, token) => {
+            return res.status(200).send({
+              message: 'success',
+              data: {
+                token,
+                // result
+                user: {
+                  id: result.id,
+                  email: result.email,
+                  // fullname: result.fullname,
+                  // role: result.role,
+                },
+              },
+            });
+          }
+        );
       })
-      .catch((err) => {
-        return res.status(500).send({ message: 'username/password salah' });
+      .catch((error) => {
+        return res.status(500).send({ message: error });
       });
   },
 
   register: (req, res) => {
-    return authModel
-      .register(req.body)
-      .then((result) => {
-        return res.status(201).send({ message: 'success', data: result });
-      })
-      .catch((err) => {
+    //tanpa bcrypt dan jwt
+    // return authModel
+    //   .register(req.body)
+    //   .then((result) => {
+    //     return res.status(201).send({ message: 'success', data: result });
+    //   })
+    //   .catch((err) => {
+    //     return res.status(500).send({ message: err.message });
+    //   });
+    console.log(req.body.password);
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+      if (err) {
         return res.status(500).send({ message: err.message });
-      });
+      } else {
+        const request = {
+          email: req.body.email,
+          password: hash,
+        };
+        return authModel
+          .register(request)
+          .then((result) => {
+            return res.status(200).send({ message: 'success', data: result });
+          })
+          .catch((error) => {
+            return res.status(500).send({ message: error });
+          });
+      }
+    });
   },
 };
 
--- src/model/auth.model.js
+++ src/model/auth.model.js
@@ -2,27 +2,52 @@
 const db = require('../helper/connection');
 const { v4: uuidv4 } = require('uuid');
 
+//deklarasi bcrypt
+const bcrypt = require('bcrypt');
+
 const authModel = {
   //data auth dll dari body
   login: ({ email, password }) => {
     //cek email dan password ada?
     console.log(email, password);
     return new Promise((resolve, reject) => {
+      //tanpa bcrypt dan jwt
+      // db.query(
+      //   `SELECT * FROM user_getpay WHERE email='${email}' AND password='${password}'`,
+      //   (err, result) => {
+      //     if (err) {
+      //       return reject({
+      //         message: err.message,
+      //       });
+      //     } else {
+      //       if (result.rows.length == 0) {
+      //         return reject('email/password salah');
+      //       } else {
+      //         return resolve(result.rows[0]);
+      //       }
+      //       // return resolve(result.rows);
+      //     }
+      //   }
+      // );
       db.query(
-        `SELECT * FROM user_getpay WHERE email='${email}' AND password='${password}'`,
+        `SELECT * FROM user_getpay WHERE email='${email}'`,
         (err, result) => {
-          if (err) {
-            return reject({
-              message: err.message,
-            });
-          } else {
-            if (result.rows.length == 0) {
-              return reject('email/password salah');
-            } else {
+          //username = unique || email =  unique
+          if (err) return reject(err.message);
+          if (result.rows.length == 0) return reject('email/password salah'); //ketika email salah
+
+          bcrypt.compare(
+            password,
+            result.rows[0].password,
+            function (err, hashingResult) {
+              //parameter dari user itu ada yang tidak valid (kosong)
+              //dari database
+              //bycript ada error yang kita tidak tahu
+              if (err) return reject('email/password salah'); //ketika kesalahan hashing(bycrpt)
+              if (!hashingResult) return reject('email/password salah'); //ketika password salah
               return resolve(result.rows[0]);
             }
-            // return resolve(result.rows);
-          }
+          );
         }
       );
     });
